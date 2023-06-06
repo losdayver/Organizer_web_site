cb1f507abac01ef44e07643e3ae44b763e9658e5
Oleg Zhmelev <99668293+losdayver@users.noreply.github.com>
2023-06-06 15:10:41 +0300

    Сделал отображение месяца и года на странице календаря
    а так же переключение стрелочками

--- main/templatetags/main_extras.py
+++ main/templatetags/main_extras.py
@@ -9,12 +9,30 @@ def get_value_from_dict(dict_data, key):
     if key:
         return dict_data.get(key)
     
-def test_if_divisible_by_7(value):
-    if (value %7 == 0):
-        return True
+def calculate_next_month(year_month:str):
+    year, month = year_month.split(' ')
+
+    month = int(month)
+    year = int(year)
+
+    if month >= 12:
+        return f'?year={year+1}&month={1}'
     
-    return False
+    return f'?year={year}&month={month + 1}'
+
+def calculate_previous_month(year_month:str):
+    year, month = year_month.split(' ')
+
+    month = int(month)
+    year = int(year)
+
+    if month <= 1:
+        return f'?year={year-1}&month={12}'
     
+    return f'?year={year}&month={month - 1}'
+
+
+register.filter('calculate_previous_month', calculate_previous_month)  
+register.filter('calculate_next_month', calculate_next_month)    
 register.filter('get_value_from_dict', get_value_from_dict)
-register.filter('test_if_divisible_by_7', test_if_divisible_by_7)
 
