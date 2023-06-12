c34b4a91628c4992b667e9bf9cbcc1cd578beb7a
svetouch <svetos_ger18@icloud.com>
2023-06-12 13:43:22 +0300

    личная страница

--- tasks_website/settings.py
+++ tasks_website/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'website3',
         'USER': 'django',
-        'PASSWORD': 'django'
+        'PASSWORD': 'qwerty'
     }
 }
 
